679f347bba5b4f6951d27923872893dd8dbebfd7
DaedeokLee <ddcalix@gmail.com>
2018-11-26 04:45:39 +0000

    0

--- 0.linux.sh
+++ 0.linux.sh
@@ -1,3 +1,8 @@
+cat >> ~/.bashrc <<_EOF_ 
 set -o vi
-
 alias ll='ls -la'
+_EOF_
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+JAVA_HOME="/usr/lib/jvm/java-8-oracle"
