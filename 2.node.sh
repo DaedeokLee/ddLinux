6fd4c270919c44ba4c452ffcad2fa8dc15647849
DaedeokLee <ddcalix@gmail.com>
2018-11-26 08:12:01 +0000

    d

--- 2.node.sh
+++ 2.node.sh
@@ -1,6 +1,5 @@
-
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
-. ~/.bashrc
+source ~/.bashrc
 nvm install stable
 
 sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
