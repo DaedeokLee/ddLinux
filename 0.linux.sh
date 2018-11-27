7ce0552adfc3c5d14ef9471b283c868924962d48
DaedeokLee <ddcalix@gmail.com>
2018-11-27 07:49:26 +0000

    rsk-setup

--- 0.linux.sh
+++ 0.linux.sh
@@ -1,8 +1,15 @@
 cat >> ~/.bashrc <<_EOF_ 
+
+####
 set -o vi
 alias ll='ls -la'
 _EOF_
-sudo add-apt-repository ppa:webupd8team/java
+source ~/.bashrc
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
-JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+sudo apt-get install \
+curl \
+unzip \
+net-utils \
+tree \
+vim \
+build-essential
