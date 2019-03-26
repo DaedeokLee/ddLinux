05ee8d025cf3179be8bf21991d1f55e9a3bd786e
DaedeokLee <ddcalix@gmail.com>
2019-03-26 08:11:33 +0000

    d

--- 1.docker.sh
+++ 1.docker.sh
@@ -16,5 +16,6 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo apt-get install -y docker-compose
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+#sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
